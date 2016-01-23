da355ff31eccd61d029d1d662da0b69b6be41dd7
Couraud <couraud.julien@gmail.com>
2016-01-23 21:41:43 +0100

    Render API Call with Promise ES6

--- client/templates/homepage/homepage.js
+++ client/templates/homepage/homepage.js
@@ -1,9 +1,16 @@
 /**
  * Created by youyou on 23/01/16.
  */
+
+Template.homepageTpl.cards = new ReactiveVar();
+
+
 Template.homepageTpl.helpers({
 
     'cards': function() {
-        return Meteor.call('getCards');
+        Meteor.call('getCards', function(err, data) {
+            Template.homepageTpl.cards.set(data);
+        });
+        return Template.homepageTpl.cards.get();
     }
 });
--- server/methods/hs-api.js
+++ server/methods/hs-api.js
@@ -1,18 +1,25 @@
 /**
  * Created by youyou on 23/01/16.
  */
+
+Future = Npm.require('fibers/future');
+
 Meteor.methods({
 
-    'getCards': function(){
-        return result = HTTP.get(
+    getCards: function() {
+        var result = new Future();
+        HTTP.get(
             "https://omgvamp-hearthstone-v1.p.mashape.com/cards/classes/Shaman?locale=frFR",
             {
-                headers:{
+                headers: {
                     "X-Mashape-Key": "XXX",
                     //"Accept": "application/json"
                 }
+            },
+            function(err,cards){
+                result.return(cards.data)
             }
-        ).data;
+        );
+        return result.wait();
     }
-
-})
+});
